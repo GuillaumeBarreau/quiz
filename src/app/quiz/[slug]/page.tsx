c3a8d8dcd1fe82187d58867e77d0cc83716c1bbb
Guillaume Barreau <guillaume.barreau@raisepartner.com>
2023-08-24 21:21:10 -0400

    update

--- src/app/quiz/[slug]/page.tsx
+++ src/app/quiz/[slug]/page.tsx
@@ -1,4 +1,4 @@
-// import Quiz from '@/components/Quiz/Quiz'
+import Quiz from '@/components/Quiz/Quiz'
 
 async function getData(quizName: string) {
     const vercel_public_domain = process.env.NEXT_PUBLIC_VERCEL_URL
@@ -8,38 +8,33 @@ async function getData(quizName: string) {
         vercel_public_domain ??
         (node_prod_env
             ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
-            : `https://${process.env.DB_HOST}:${process.env.DB_PORT}`)
+            : `http://${process.env.DB_HOST}:${process.env.DB_PORT}`)
 
-    const questionsFetch = await fetch(
-        `https://quiz-git-fix-buildenv-guillaumebarreau.vercel.app/api/quiz/${quizName}`
-    )
+    const questionsFetch = await fetch(`${DOMAIN}/api/quiz/${quizName}`)
 
     const questions = await questionsFetch.json()
 
     return {
         questions,
-        domainA: DOMAIN,
-        domainB: process.env.NEXT_PUBLIC_VERCEL_URL,
-        domainC:
-            'https://quiz-hgckelvsq-guillaumebarreau.vercel.app/api/quiz/professional-scrum-developer-1',
+        domain: `${DOMAIN}/api/quiz/${quizName}`,
     }
 }
 
 const Page: React.FC<{ params: { slug: string } }> = async ({ params }) => {
     const quizName = params?.slug
-    const { questions, domainA, domainB, domainC } = await getData(quizName)
+    const { questions, domain } = await getData(quizName)
 
-    // if (!questions) {
-    //     return <p>Not data found</p>
-    // }
-    console.log(questions)
+    if (!questions) {
+        return <p>Not data found</p>
+    }
 
     return (
         <>
-            <p>Domain A: {domainA}</p>
-            <p>Domain B: {domainB}</p>
-            <p>Domain C: {domainC}</p>
-            {/* <p>Quiz name: {questions}</p> */}
+            <p>{domain}</p>
+            <Quiz
+                questionsArray={questions?.data}
+                questionsNumber={questions?.data?.length}
+            />
         </>
     )
 }
