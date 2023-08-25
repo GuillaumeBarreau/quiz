6689e1753065f6e96a9f6481d13581cee7ab44c3
Guillaume Barreau <guillaume.barreau@raisepartner.com>
2023-08-24 21:49:31 -0400

    update

--- src/app/quiz/[slug]/page.tsx
+++ src/app/quiz/[slug]/page.tsx
@@ -2,13 +2,9 @@ import Quiz from '@/components/Quiz/Quiz'
 
 async function getData(quizName: string) {
     const vercel_public_domain = process.env.NEXT_PUBLIC_VERCEL_URL
-    const node_prod_env = process.env.NODE_ENV === 'production'
 
     const DOMAIN =
-        vercel_public_domain ??
-        (node_prod_env
-            ? `${process.env.NEXT_PUBLIC_VERCEL_URL}`
-            : `${process.env.DB_HOST}:${process.env.DB_PORT}`)
+        vercel_public_domain ?? `${process.env.DB_HOST}:${process.env.DB_PORT}`
 
     try {
         const settings = {
@@ -27,35 +23,27 @@ async function getData(quizName: string) {
 
         return {
             questions,
-            domain: `${DOMAIN}/api/quiz/${quizName}`,
-            domainB: vercel_public_domain,
         }
     } catch (error) {
         return {
             questions: [],
-            domain: `${DOMAIN}/api/quiz/${quizName}`,
-            domainB: vercel_public_domain,
         }
     }
 }
 
 const Page: React.FC<{ params: { slug: string } }> = async ({ params }) => {
     const quizName = params?.slug
-    const { questions, domain, domainB } = await getData(quizName)
+    const { questions } = await getData(quizName)
 
     if (!questions) {
         return <p>Not data found</p>
     }
 
     return (
-        <>
-            <p>{domain}</p>
-            <p>{domainB}</p>
-            <Quiz
-                questionsArray={questions?.data}
-                questionsNumber={questions?.data?.length}
-            />
-        </>
+        <Quiz
+            questionsArray={questions?.data}
+            questionsNumber={questions?.data?.length}
+        />
     )
 }
 
