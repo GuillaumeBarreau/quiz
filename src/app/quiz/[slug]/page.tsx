859268b0c133e7cb5e2fe738aa732753bb349a8e
Guillaume Barreau <guillaume.barreau@raisepartner.com>
2023-08-24 21:03:48 -0400

    test

--- src/app/quiz/[slug]/page.tsx
+++ src/app/quiz/[slug]/page.tsx
@@ -10,12 +10,18 @@ async function getData(quizName: string) {
             ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
             : `https://${process.env.DB_HOST}:${process.env.DB_PORT}`)
 
-    const questionsFetch = await fetch(`${DOMAIN}/api/quiz/${quizName}`)
+    const questionsFetch = await fetch(
+        `quiz-git-fix-buildenv-guillaumebarreau.vercel.app/api/quiz/professional-scrum-developer-1/api/quiz/${quizName}`
+    )
 
     const questions = await questionsFetch.json()
 
     return {
         questions,
+        domainA: DOMAIN,
+        domainB: process.env.NEXT_PUBLIC_VERCEL_URL,
+        domainC:
+            'http://quiz-hgckelvsq-guillaumebarreau.vercel.app/api/quiz/professional-scrum-developer-1',
     }
 }
 
