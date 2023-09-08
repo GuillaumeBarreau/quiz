6fef3a9d9ec4fd7be56df32db17ced4879302f87
Guillaume Barreau <43338251+GuillaumeBarreau@users.noreply.github.com>
2023-09-07 23:52:52 -0400

    refactor: :recycle: Updated generateData scripts (#30)
    Co-authored-by: Guillaume Barreau <guillaume.barreau@raisepartner.com>

--- scripts/data.config.mjs
+++ scripts/data.config.mjs
@@ -3,21 +3,27 @@ import fs from 'fs'
 const dataObjectTemplate = () => {
     const configDir = 'config'
     try {
-        const files = fs.readdirSync(configDir)
-
-        const config = files.map((file) => {
-            const fileBuffer = fs.readFileSync(`./config/${file}`, 'utf-8')
-            const fileJson = JSON.parse(fileBuffer)
-
-            return {
-                description: fileJson.description,
-                longTitle: fileJson.longTitle,
-                shortTitle: fileJson.shortTitle,
-                url: fileJson.url,
-                get path() {
-                    return `./data/${this.shortTitle}.md`
-                },
-            }
+        const folders = fs.readdirSync(configDir)
+        const config = []
+        folders.forEach((folder) => {
+            const files = fs.readdirSync(`${configDir}/${folder}`)
+            return files.forEach((file) => {
+                const fileBuffer = fs.readFileSync(
+                    `./config/${folder}/${file}`,
+                    'utf-8'
+                )
+                const fileJson = JSON.parse(fileBuffer)
+                config.push({
+                    category: folder,
+                    description: fileJson.description,
+                    longTitle: fileJson.longTitle,
+                    shortTitle: fileJson.shortTitle,
+                    url: fileJson.url,
+                    get path() {
+                        return `./data/${folder}/${this.shortTitle}.md`
+                    },
+                })
+            })
         })
 
         return config
