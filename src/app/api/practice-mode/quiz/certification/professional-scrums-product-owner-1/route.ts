860bac5b643050dc935a0664c656c60a79c3d0a9
Guillaume Barreau <guillaume.barreau@raisepartner.com>
2023-09-13 20:58:08 -0400

    refactor(scripts): :recycle: refactor scripts ganerate Data

--- src/app/api/practice-mode/quiz/certification/professional-scrums-product-owner-1/route.ts
+++
@@ -1,9 +0,0 @@
-
-import { NextApiResponse } from 'next'
-import { getFileData } from '@/app/api/api.utils'
-const dataFilePath = "data/certification/PSPO_01/index.json"
-
-export async function GET(req: Request, res: NextApiResponse) {
-    const questionCount = 10
-    return getFileData(req, res, dataFilePath, questionCount)
-}
