33b5bfcb3c1ea38b34b58ae372e34cabecaa46d8
Guillaume Barreau <43338251+GuillaumeBarreau@users.noreply.github.com>
2023-09-20 23:46:28 -0400

    feat(home): :lipstick: Add animation for ProgressBar component (#45)
    Co-authored-by: Guillaume Barreau <guillaume.barreau@raisepartner.com>

--- src/components/_ProgressBar/VerticalBar/VerticalBar.component.tsx
+++ src/components/_ProgressBar/VerticalBar/VerticalBar.component.tsx
@@ -7,7 +7,7 @@ const VerticalBar: React.FC<IVerticalBar> = (props) => {
         <div className="w-full bg-gray-200 dark:bg-gray-700">
             <div
                 data-testid="VerticalBar_progress"
-                className="bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none h-3"
+                className="transition-all duration-300 bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none h-3"
                 style={{
                     width: `${progress}%`,
                 }}
